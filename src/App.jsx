d6e55e29ce932f0608800c6d1b743121e53ac670
espenb08 <espenb08.kodehode@gmail.com>
2024-01-31 09:43:54 +0100

    finished drop down menu

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import KommuneListe from './Components/api/APIpath';
+
+
+function App() {
+
+  return (
+    <>
+  <KommuneListe></KommuneListe>
+  <p>hei</p>
+   </>
+    
+  )
+}
+
+export default App
---
+++ src/Components/api/data.jsx
@@ -0,0 +1,1 @@
+export const data = () =>{"https://data.ssb.no/api/klass/v1/versions/1847.json"}
