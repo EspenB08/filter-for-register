81ea84dea386e649ade35bb55064d1cb61f0efff
EspenB08 <142892271+EspenB08@users.noreply.github.com>
2024-02-01 23:43:01 +0100

    removed button

--- src/Components/buttons/button.jsx
+++
@@ -1,9 +0,0 @@
-import {Button} from react-bootstrap
-
-function button(){
-    return(
-        
-        <button className="test" onClick={alert}>Finn Bedrift</button>
-    )
-}
-export default Button
