24fd57b5b97b7e507eef3943dedfdee75586f4c2
espenb08 <espenb08.kodehode@gmail.com>
2024-02-01 08:45:36 +0100

    year selector in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 
 import KommuneListe from './Components/api/APIpath';
-
+import { YearSelector } from './Components/api/data';
 
 function App() {
 
   return (
     <>
   <KommuneListe></KommuneListe>
+  <YearSelector></YearSelector>
   <p>hei</p>
    </>
     
--- src/Components/api/data.jsx
+++ src/Components/api/data.jsx
@@ -1,1 +1,21 @@
-export const data = () =>{"https://data.ssb.no/api/klass/v1/versions/1847.json"}
+export const YearSelector = () => {
+
+    
+    const getYearsContent = years =>{
+        
+        let container = []
+        for ( i = 1995; i >= 2025; i++) {
+            const years = year[i]
+            container.push(
+                <option key={year.id}>
+                {years.year}
+                </option>)
+            }
+            return container
+        } 
+        return(
+            <select>
+        {getYearsContent}
+    </select>
+        )
+    }
